c6fdc3d4672815b0711fd9e3fcb7599b0d4f4cae
xColdNox <102612330+xColdNox@users.noreply.github.com>
2022-05-15 07:06:22 +0200

    Add files via upload

---
+++ PASS GENERATOR.py
@@ -0,0 +1,24 @@
+import random
+
+
+lowercase = "qwertyuiopasdfghjklzxcvbnm"
+uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM"
+numbers = "1234567890"
+symbols = "!@#$%^&*=+"
+
+# below I create a password randomized from all the categories
+
+generated = lowercase + uppercase + numbers + symbols
+passwordlen = 10
+password = "".join(random.sample(generated, passwordlen))
+
+# below I create a password which has letters first and symbols + numbers later for easier memorization
+
+generate_letters = lowercase + uppercase
+generate_num_symb = numbers + symbols
+passwordletterlen = 6
+password_num_symb_len = 4
+passwordeasier = "".join(random.sample(generate_letters, passwordletterlen) + random.sample(generate_num_symb, password_num_symb_len))
+
+print("Your unbreakable password is: " + password)
+print("However if you want an easier to remember password, there you go: " + passwordeasier)
